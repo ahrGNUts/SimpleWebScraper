915118a74fbba04263410fb5cb5206e0f647be1a
Patrick <revmydev@gmail.com>
2019-07-15 20:00:26 -0700

    built out Builders and subsequent classes for program

---
+++ SimpleWebScraper/Builders/ScrapeCriteriaPartBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using SimpleWebScraper.Data;
+
+namespace SimpleWebScraper.Builders
+{
+    public class ScrapeCriteriaPartBuilder
+    {
+        private string _regex;
+        private RegexOptions _regexOptions;
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            SetDefaults();
+        }
+
+        private void SetDefaults()
+        {
+            _regex = String.Empty;
+            _regexOptions = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOptions(RegexOptions regexOptions)
+        {
+            _regexOptions = regexOptions;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOptions;
+
+            return scrapeCriteriaPart;
+        }
+    }
+}
+
