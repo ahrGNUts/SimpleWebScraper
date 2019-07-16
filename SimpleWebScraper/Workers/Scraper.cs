e4510c64718049ba6f112c4124fb43d8cbf830da
Patrick <revmydev@gmail.com>
2019-07-15 20:24:41 -0700

    fleshing out Scraper

---
+++ SimpleWebScraper/Workers/Scraper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleWebScraper.Data;
+using System.Text.RegularExpressions;
+
+namespace SimpleWebScraper.Workers
+{
+    public class Scraper
+    {
+        public List<string> Scrape(ScrapeCriteria scrapeCriteria)
+        {
+            List<string> scrapedElements = new List<string>();
+
+            MatchCollection matches = Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.regexOptions);
+
+            foreach(Match match in matches)
+            {
+                if(scrapeCriteria.Parts.Count == 0)
+                {
+                    scrapedElements.Add(match.Groups[0].Value);
+                }
+                else
+                {
+                    foreach (var part in scrapeCriteria.Parts)
+                    {
+                        Match matchedPart = Regex.Match(match.Groups[0].Value, part.Regex, part.RegexOption);
+
+                        if (matchedPart.Success)
+                            scrapedElements.Add(matchedPart.Groups[1].Value);
+                    }
+                }
+            }
+
+            return scrapedElements;
+        }
+    }
+}
+
